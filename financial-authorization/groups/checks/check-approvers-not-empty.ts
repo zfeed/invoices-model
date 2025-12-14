ed7c9d2c8367dcfca680baad847d628b4fe32cf1
Roman Rodionov <rodionov2296@gmail.com>
2025-12-14 13:18:30 +0400

    refactor(result): enhance Result type and update approval group validation

--- financial-authorization/groups/checks/check-approvers-not-empty.ts
+++ financial-authorization/groups/checks/check-approvers-not-empty.ts
@@ -1,17 +1,31 @@
-import { isEmpty } from 'ramda';
+import { ifElse, isEmpty } from 'ramda';
 import { DOMAIN_ERROR_CODE } from '../../../building-blocks/errors/domain/domain-codes';
 import { DomainError } from '../../../building-blocks/errors/domain/domain.error';
+import { Result } from '../../../building-blocks/result';
+import { Approval } from '../../approval/approval';
 import { Approver } from '../../approver/approver';
 
-export function checkApproversNotEmpty(
-    approvers: Approver[]
-): DomainError | null {
-    if (isEmpty(approvers)) {
-        return new DomainError({
+type GroupInput = {
+    approvers: Approver[];
+    approvals: Approval[];
+};
+
+const approversEmpty = (data: GroupInput) => isEmpty(data.approvers);
+const createApproversEmptyError = () =>
+    Result.error(
+        new DomainError({
             message: 'Approvers array cannot be empty',
             code: DOMAIN_ERROR_CODE.FINANCIAL_AUTHORIZATION_APPROVERS_EMPTY,
-        });
-    }
+        })
+    );
+const createApproversNotEmptyResult = (data: GroupInput) => Result.ok(data);
 
-    return null;
+export function approversNotEmpty(data: GroupInput) {
+    return Result.ok<DomainError, GroupInput>(data).flatMap(
+        ifElse(
+            approversEmpty,
+            createApproversEmptyError,
+            createApproversNotEmptyResult
+        )
+    );
 }
