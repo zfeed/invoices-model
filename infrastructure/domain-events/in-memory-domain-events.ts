405ccc1b2629247adc3727302cbbd9112db61ded
Roman Rodionov <rodionov2296@gmail.com>
2026-02-16 16:42:25 +0400

    feat: add in-memory domain events bus and domain events tests

---
+++ infrastructure/domain-events/in-memory-domain-events.ts
@@ -0,0 +1,29 @@
+import { DomainEvent } from '../../building-blocks/events/domain-event';
+import { PublishableEvents } from '../../building-blocks/events/event-publisher.interface';
+import { DomainEvents } from '../../invoices/application/domain-events/domain-events.interface';
+
+type EventHandler<T = any> = (event: T) => void;
+
+export class InMemoryDomainEvents implements DomainEvents {
+    private handlers = new Map<Function, EventHandler[]>();
+
+    publishEvents(...objects: PublishableEvents<DomainEvent<unknown>>[]): void {
+        for (const object of objects) {
+            for (const event of object.events) {
+                const handlers = this.handlers.get(event.constructor) ?? [];
+                for (const handler of handlers) {
+                    handler(event);
+                }
+            }
+        }
+    }
+
+    subscribeToEvent<T extends DomainEvent<D>, D>(
+        eventClass: new (...args: any[]) => T,
+        handler: (event: T) => void,
+    ): void {
+        const handlers = this.handlers.get(eventClass) ?? [];
+        handlers.push(handler);
+        this.handlers.set(eventClass, handlers);
+    }
+}
