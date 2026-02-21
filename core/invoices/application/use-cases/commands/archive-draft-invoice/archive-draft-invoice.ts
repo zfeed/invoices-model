9ef0d31d11239842bafd0d174cc792fc13064f97
Roman Rodionov <rodionov2296@gmail.com>
2026-02-21 18:42:26 +0400

    feat(invoices): add archive-draft-invoice and draft-draft-invoice use cases

---
+++ core/invoices/application/use-cases/commands/archive-draft-invoice/archive-draft-invoice.ts
@@ -0,0 +1,38 @@
+import { APPLICATION_ERROR_CODE } from '../../../../../../building-blocks/errors/application/application-codes';
+import { ApplicationError } from '../../../../../../building-blocks/errors/application/application.error';
+import { DraftInvoice } from '../../../../domain/draft-invoice/draft-invoice';
+import { Id } from '../../../../domain/id/id';
+import { DomainEvents } from '../../../../../shared/domain-events/domain-events.interface';
+import { UnitOfWorkFactory } from '../../../unit-of-work/unit-of-work.interface';
+
+export class ArchiveDraftInvoice {
+    constructor(
+        private readonly unitOfWorkFactory: UnitOfWorkFactory,
+        private readonly domainEvents: DomainEvents
+    ) {}
+
+    public async execute(id: string) {
+        const draftInvoice = await this.unitOfWorkFactory.start(
+            async (unitOfWork) => {
+                const draftInvoice = await unitOfWork
+                    .collection(DraftInvoice)
+                    .get(Id.fromString(id));
+
+                if (!draftInvoice) {
+                    throw new ApplicationError({
+                        message: 'Draft invoice not found',
+                        code: APPLICATION_ERROR_CODE.ITEM_NOT_FOUND,
+                    });
+                }
+
+                draftInvoice.archive().unwrap();
+
+                return draftInvoice;
+            }
+        );
+
+        await this.domainEvents.publishEvents(draftInvoice);
+
+        return draftInvoice.toPlain();
+    }
+}
