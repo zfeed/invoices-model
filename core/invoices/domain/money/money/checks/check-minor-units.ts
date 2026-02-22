62fb60b90a0dbdfa49e0b549f8f8ae21b8952aae
Roman Rodionov <rodionov2296@gmail.com>
2026-02-22 13:15:31 +0400

    refactor(invoices): update check-minor-units validation

--- core/invoices/domain/money/money/checks/check-minor-units.ts
+++ core/invoices/domain/money/money/checks/check-minor-units.ts
@@ -2,35 +2,39 @@ import { isInt } from 'validator';
 import { DOMAIN_ERROR_CODE, DomainError } from '../../../../../../building-blocks';
 import { Numeric } from '../../../numeric/numeric';
 
+const ZERO = Numeric.create('0').unwrap();
+
 export function checkMinorUnits(units: string | Numeric): DomainError | null {
-    const minorUnitsNotIntegerError = new DomainError({
-        message: `Invalid minor units. Expected an integer, received: ${units}`,
-        code: DOMAIN_ERROR_CODE.MONEY_MINOR_UNITS_NOT_INTEGER,
-    });
-
-    const minorUnitsNotGteZeroError = new DomainError({
-        message: `Invalid minor units. Expected a non-negative integer, received: ${units}`,
-        code: DOMAIN_ERROR_CODE.MONEY_MINOR_UNITS_NOT_GTE_ZERO,
-    });
-
     if (typeof units === 'string') {
         if (!isInt(units, { allow_leading_zeroes: false })) {
-            return minorUnitsNotIntegerError;
+            return new DomainError({
+                message: `Invalid minor units. Expected an integer, received: ${units}`,
+                code: DOMAIN_ERROR_CODE.MONEY_MINOR_UNITS_NOT_INTEGER,
+            });
         }
 
         if (Number(units) < 0) {
-            return minorUnitsNotGteZeroError;
+            return new DomainError({
+                message: `Invalid minor units. Expected a non-negative integer, received: ${units}`,
+                code: DOMAIN_ERROR_CODE.MONEY_MINOR_UNITS_NOT_GTE_ZERO,
+            });
         }
 
         return null;
     }
 
     if (units.decimalPlaces() > 0) {
-        return minorUnitsNotIntegerError;
+        return new DomainError({
+            message: `Invalid minor units. Expected an integer, received: ${units}`,
+            code: DOMAIN_ERROR_CODE.MONEY_MINOR_UNITS_NOT_INTEGER,
+        });
     }
 
-    if (units.lessThan(Numeric.create('0').unwrap())) {
-        return minorUnitsNotGteZeroError;
+    if (units.lessThan(ZERO)) {
+        return new DomainError({
+            message: `Invalid minor units. Expected a non-negative integer, received: ${units}`,
+            code: DOMAIN_ERROR_CODE.MONEY_MINOR_UNITS_NOT_GTE_ZERO,
+        });
     }
 
     return null;
