ecd5f554b1540cf0d5d31d20658c6ad4608ba130
Roman Rodionov <rodionov2296@gmail.com>
2026-03-01 14:33:42 +0400

    feat(invoices): add currency, money, and numeric data mappers

---
+++ src/infrastructure/persistent-manager/mappers/numeric.data-mapper.ts
@@ -0,0 +1,24 @@
+import { Numeric } from '../../../core/invoices/domain/numeric/numeric';
+
+export type NumericRecord = {
+    value: string;
+};
+
+export class NumericDataMapper extends Numeric {
+    static from(numeric: Numeric): NumericDataMapper {
+        return Object.setPrototypeOf(
+            numeric,
+            NumericDataMapper.prototype
+        ) as NumericDataMapper;
+    }
+
+    static fromRecord(record: NumericRecord): NumericDataMapper {
+        return new NumericDataMapper(record.value);
+    }
+
+    toRecord(): NumericRecord {
+        return {
+            value: this._value.toString(),
+        };
+    }
+}
