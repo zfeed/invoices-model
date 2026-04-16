d64be0cf118734ba262ab3719fc7e5f7dc89e8cc
Roman Rodionov <rodionov2296@gmail.com>
2026-04-16 17:39:09 +0400

    test: enable vitest typecheck

--- vitest.config.ts
+++ vitest.config.ts
@@ -9,5 +9,11 @@ export default defineConfig({
         setupFiles: ['dotenv/config'],
         globalSetup: ['test/global-setup.ts'],
         fileParallelism: false,
+        typecheck: {
+            enabled: true,
+            checker: 'tsc',
+            tsconfig: './tsconfig.json',
+            include: ['src/**/*.test.ts', 'src/**/*.spec.ts', 'e2e/**/*.spec.ts'],
+        },
     },
 });
